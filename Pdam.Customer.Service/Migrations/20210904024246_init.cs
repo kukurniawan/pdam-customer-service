ca061216bb3111ece3cfdfe604a1df85fc249c30
kunkun kurniawan <kurnia.wirawan@gmail.com>
2021-09-04 09:45:11 +0700

    init migrations

---
+++ Pdam.Customer.Service/Migrations/20210904024246_init.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pdam.Customer.Service.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Router",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatus = table.Column<byte>(type: "smallint", nullable: false),
+                    CompanyCode = table.Column<string>(type: "text", nullable: true),
+                    BranchCode = table.Column<string>(type: "text", nullable: true),
+                    GroupRoute = table.Column<string>(type: "text", nullable: true),
+                    ZoneRoute = table.Column<string>(type: "text", nullable: true),
+                    RouteCode = table.Column<string>(type: "text", nullable: true),
+                    RouteName = table.Column<string>(type: "text", nullable: true),
+                    CodeReader = table.Column<string>(type: "text", nullable: true),
+                    DayInMonth = table.Column<int>(type: "integer", nullable: true),
+                    UserProfileId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IndexReading = table.Column<int>(type: "integer", nullable: false),
+                    Village = table.Column<string>(type: "text", nullable: true),
+                    District = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Router", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatus = table.Column<byte>(type: "smallint", nullable: false),
+                    CompanyCode = table.Column<string>(type: "text", nullable: true),
+                    BranchCode = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    CustomerName = table.Column<string>(type: "text", nullable: true),
+                    CustomerCode = table.Column<string>(type: "text", nullable: true),
+                    CustomerGroupCode = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    RouterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_Router_RouterId",
+                        column: x => x.RouterId,
+                        principalTable: "Router",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAddress",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatus = table.Column<byte>(type: "smallint", nullable: false),
+                    AddressLine1 = table.Column<string>(type: "text", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "text", nullable: true),
+                    PostalCode = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    State = table.Column<string>(type: "text", nullable: true),
+                    Country = table.Column<string>(type: "text", nullable: true),
+                    District = table.Column<string>(type: "text", nullable: true),
+                    Village = table.Column<string>(type: "text", nullable: true),
+                    RW = table.Column<string>(type: "text", nullable: true),
+                    RT = table.Column<string>(type: "text", nullable: true),
+                    Latitude = table.Column<double>(type: "double precision", nullable: true),
+                    Longitude = table.Column<double>(type: "double precision", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAddress", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerAddress_Customers_Id",
+                        column: x => x.Id,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAssets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatus = table.Column<byte>(type: "smallint", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StatusDescription = table.Column<string>(type: "text", nullable: true),
+                    SerialNumber = table.Column<string>(type: "text", nullable: true),
+                    AssetDescription = table.Column<string>(type: "text", nullable: true),
+                    DateOfInstallation = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    InstalledBy = table.Column<string>(type: "text", nullable: true),
+                    InstalledDocument = table.Column<string>(type: "text", nullable: true),
+                    DateOfWithdraw = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    WithdrawBy = table.Column<string>(type: "text", nullable: true),
+                    WithdrawDocument = table.Column<string>(type: "text", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAssets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerAssets_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerContacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatus = table.Column<byte>(type: "smallint", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactPerson = table.Column<string>(type: "text", nullable: true),
+                    PhoneLine1 = table.Column<string>(type: "text", nullable: true),
+                    PhoneLine2 = table.Column<string>(type: "text", nullable: true),
+                    Fax = table.Column<string>(type: "text", nullable: true),
+                    MobilePhone1 = table.Column<string>(type: "text", nullable: true),
+                    MobilePhone2 = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerContacts_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerStatusLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RowStatusOldValue = table.Column<byte>(type: "smallint", nullable: false),
+                    RowStatusNewValue = table.Column<byte>(type: "smallint", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerStatusLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerStatusLogs_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAssets_CustomerId",
+                table: "CustomerAssets",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerContacts_CustomerId",
+                table: "CustomerContacts",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CompanyCode_CustomerCode",
+                table: "Customers",
+                columns: new[] { "CompanyCode", "CustomerCode" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_RouterId",
+                table: "Customers",
+                column: "RouterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerStatusLogs_CustomerId",
+                table: "CustomerStatusLogs",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Router_CompanyCode_RouteCode",
+                table: "Router",
+                columns: new[] { "CompanyCode", "RouteCode" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerAddress");
+
+            migrationBuilder.DropTable(
+                name: "CustomerAssets");
+
+            migrationBuilder.DropTable(
+                name: "CustomerContacts");
+
+            migrationBuilder.DropTable(
+                name: "CustomerStatusLogs");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Router");
+        }
+    }
+}
